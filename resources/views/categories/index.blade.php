7c3e40f91ac60e6a93c2cf288154e5de24ef9cea
Med Aduh <im.ramidem@gmail.com>
2020-08-14 15:36:26 +0800

    Read - categories index page

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-12 rounded-lg border border-info p-3">
+            <a
+                href="{{ route('categories.create')}}"
+                class="btn btn-primary btn-sm">
+                Add Category
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 py-3">
+            @foreach($categories as $category)
+                <h2>{{ $category->name }}</h2>
+            @endforeach
+        </div>
+    </div>
+@endsection
